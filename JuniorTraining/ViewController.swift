1c318b29885e876ec07cd8456199f6028f0c9cb6
Bui Minh Nhut <nhut.bm@neo-lab.vn>
2021-05-29 09:49:20 +0700

    setup project

---
+++ JuniorTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuniorTraining
+//
+//  Created by Nhut Bui on 5/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
